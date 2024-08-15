22baee3866b8d23b97ba6ea21dc39f71ab2b8f45
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-15 13:29:10 -0400

    opjknm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        printMegaBytesAndKilobytes(2500);
+        printMegaBytesAndKilobytes(-1024);
+        printMegaBytesAndKilobytes(5000);
+    }
+
+    public static void printMegaBytesAndKilobytes(int kilobytes){
+        if () {
+            int mega = kilobytes / 1024;
+            int kb = kilobytes % 1024;
+            System.out.println(kilobytes + " KB = " + mega + " MB and " + kb + " KB ");
+            // String result = String.format("%s KB = %s MB and %s KB", kilobytes, mega, kb);
+            // System.out.println(result);
+        } else {
+            System.out.println("Invaild Value");
+        }
+    }
+
 }
